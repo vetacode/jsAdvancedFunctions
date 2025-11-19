9bac70e6b6ba064ae108dd61ecbe5b314c3cb33f
vetacode <vetacode@gmail.com>
2025-11-19 15:41:50 +0700

    adding closure

--- variableScopeClosure.js
+++ variableScopeClosure.js
@@ -54,4 +54,33 @@ console.log(counter()); // 2
  * 2. function declaration is also a value, which instantly fully initialized
  * 3. Innner and outer lexical environment
  * 4. returning a function
+ * function has a hidden prop named [[Environment]] that keeps the reference to the lexical env where the function was created
  */
+
+//CLOSURE
+//is a function that remembers its outer variables and can access them.
+//All functions in JS is a closure: they remember where they were created (using [[Environment]] prop), and can access outer variables
+
+//GARBAGE COLLECTION
+function f() {
+  let value = 123;
+
+  return function () {
+    console.log(value);
+  };
+}
+
+let g = f(); // g.[[Environment]] stores a reference to the Lexical Environment
+// of the corresponding f() call
+
+function f2() {
+  let value = Math.random();
+
+  return function () {
+    console.log(value);
+  };
+}
+
+// 3 functions in array, every one of them links to Lexical Environment
+// from the corresponding f() run
+let arr = [f2(), f2(), f2()];
