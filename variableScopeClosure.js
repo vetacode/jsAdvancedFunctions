f43799eec047f2bb3538dfb0becbeec079bb27d4
vetacode <vetacode@gmail.com>
2025-11-19 12:28:34 +0700

    adding nested functions

--- variableScopeClosure.js
+++ variableScopeClosure.js
@@ -1,4 +1,4 @@
-//SCOPE
+//CODE BLOCKS
 {
   let say = 'Hello';
   console.log(say);
@@ -19,3 +19,30 @@ for (let i = 0; i < 5; i++) {
   console.log(i);
 }
 // console.log(i); //error, even visually i is outside the scope {}, but its considered as part of {} scope
+
+//NESTED FUNCTIONS
+function sayHiBye(firstName, lastName) {
+  // helper nested function to use below
+  function getFullName() {
+    return firstName + ' ' + lastName;
+  }
+
+  console.log('Hello, ' + getFullName());
+  console.log('Bye, ' + getFullName());
+}
+sayHiBye('Steve', 'Jonan');
+
+//function returning function
+function makeCounter() {
+  let count = 0;
+
+  return function () {
+    return count++;
+  };
+}
+
+let counter = makeCounter();
+
+console.log(counter()); // 0
+console.log(counter()); // 1
+console.log(counter()); // 2
