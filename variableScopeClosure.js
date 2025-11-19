20df6cf861d1da6f825d573d66b747ff6cb1541f
vetacode <vetacode@gmail.com>
2025-11-19 12:21:04 +0700

    adding scope

---
+++ variableScopeClosure.js
@@ -0,0 +1,16 @@
+//SCOPE
+{
+  let say = 'Hello';
+  console.log(say);
+}
+
+{
+  let say = 'Goodby';
+  console.log(say);
+}
+
+if (true) {
+  let i = 5;
+  console.log(i); //5
+}
+// console.log(i); //error, out of schope
