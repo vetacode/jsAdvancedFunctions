c38b798a59df10b541b08b6d4177ab89f0b8bca0
vetacode <vetacode@gmail.com>
2025-11-19 11:20:52 +0700

    adding copy an Object with spread

--- restParamSpread.js
+++ restParamSpread.js
@@ -136,3 +136,21 @@ console.log(arrNum); // 1, 2, 3, 4
 console.log(arrCopy); // 1, 2, 3
 arrCopy.push(4);
 console.log(arrCopy); // 1, 2, 3, 4
+
+//COPY an OBJECT
+let obj = { a: 1, b: 2, c: 3 };
+
+let objCopy = { ...obj }; // spread the object into a list of parameters
+// then return the result in a new object
+console.log(objCopy);
+
+// do the objects have the same contents?
+console.log(JSON.stringify(obj) === JSON.stringify(objCopy)); // true
+
+// are the objects equal?
+console.log(obj === objCopy); // false (not same reference)
+
+// modifying our initial object does not modify the copy:
+obj.d = 4;
+console.log(JSON.stringify(obj)); // {"a":1,"b":2,"c":3,"d":4}
+console.log(JSON.stringify(objCopy)); // {"a":1,"b":2,"c":3}
