17d110043d773242f7ce7fb05f717c0d96bb2667
vetacode <vetacode@gmail.com>
2025-11-19 12:23:28 +0700

    adding code blocks

--- variableScopeClosure.js
+++ variableScopeClosure.js
@@ -14,3 +14,8 @@ if (true) {
   console.log(i); //5
 }
 // console.log(i); //error, out of schope
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+// console.log(i); //error, even visually i is outside the scope {}, but its considered as part of {} scope
