0bdb28673abb3f39d48915e701bf7338d1e35246
vetacode <vetacode@gmail.com>
2025-11-20 19:48:47 +0700

    task 10 done

--- variableScopeClosure.js
+++ variableScopeClosure.js
@@ -522,3 +522,30 @@ Why do all of the shooters show the same value?
 
 Fix the code so that they work as intended.
  */
+
+function makeArmy() {
+  let shooters = [];
+
+  let i = 0;
+  while (i <= 10) {
+    let j = i;
+    let shooter = function () {
+      // create a shooter function,
+      console.log(j); // that should show its number
+    };
+    shooters.push(shooter); // and add it to the array
+    i++;
+  }
+
+  // ...and return the array of shooters
+  return shooters;
+}
+
+let army = makeArmy();
+
+// all shooters show 10 instead of their numbers 0, 1, 2, 3...
+console.log(army[0]()); // 10 from the shooter number 0
+army[1](); // 10 from the shooter number 1
+army[2](); // 10 ...and so on.
+army[7](); // 10 ...and so on.
+army[10](); // 10 ...and so on.
