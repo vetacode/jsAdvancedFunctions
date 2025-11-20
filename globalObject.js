a21ce632c7dac2d7b97e68341ea1e5d9fff758ce
vetacode <vetacode@gmail.com>
2025-11-20 20:32:24 +0700

    completing global object

--- globalObject.js
+++ globalObject.js
@@ -7,3 +7,16 @@ global.console.log('Hello');
 var gVar = 5;
 
 console.log(global.gVar); // 5 (became a property of the global object)
+
+//Cannot using let
+let gLet = 5;
+alert(window.gLet); // undefined (doesn't become a property of the global object)
+
+//Using for polyfills
+if (!window.Promise) {
+  alert('Your browser is really old!');
+}
+
+if (!window.Promise) {
+  // window.Promise = ... // custom implementation of the modern language feature
+}
