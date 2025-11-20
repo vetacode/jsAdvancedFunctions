7c60af3e369b629210f1fd9589259f64a6864d48
vetacode <vetacode@gmail.com>
2025-11-20 21:44:30 +0700

    task 1 done

--- functionObjectNFE.js
+++ functionObjectNFE.js
@@ -135,3 +135,44 @@ let welcome2 = sayHi6;
 sayHi6 = null;
 
 welcome2(); // Hello, Guest (nested call works)
+
+/**TASK 1
+ * Set and decrease for counter
+importance: 5
+Modify the code of makeCounter() so that the counter can also decrease and set the number:
+
+counter() should return the next number (as before).
+counter.set(value) should set the counter to value.
+counter.decrease() should decrease the counter by 1.
+See the sandbox code for the complete usage example.
+
+P.S. You can use either a closure or the function property to keep the current count. Or write both variants.
+ */
+
+function makeCounter2() {
+  let count = 0;
+
+  function counter() {
+    return ++count;
+  }
+
+  counter.set = (value) => (count = value);
+
+  counter.decrease = () => --count;
+
+  return counter;
+}
+
+let counter3 = makeCounter2();
+
+console.log(counter3());
+console.log(counter3());
+console.log(counter3());
+console.log(counter3());
+console.log(counter3.decrease());
+console.log(counter3.decrease());
+console.log(counter3.decrease());
+console.log(counter3.decrease());
+console.log(counter3.decrease());
+console.log(counter3.decrease());
+console.log(counter3.set(10));
