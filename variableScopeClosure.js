6992df8e1e17b01da70f52891134d20e128c6db0
vetacode <vetacode@gmail.com>
2025-11-20 16:51:11 +0700

    task 2 accomplished

--- variableScopeClosure.js
+++ variableScopeClosure.js
@@ -188,3 +188,44 @@ function sayHi() {
 sayHi(); //Hi, John
 name = 'Pete';
 sayHi(); //Hi, Pete
+
+/**TASK 2
+ * Which variables are available?
+importance: 5
+The function makeWorker below makes another function and returns it. That new function can be called from somewhere else.
+
+Will it have access to the outer variables from its creation place, or the invocation place, or both?
+
+function makeWorker() {
+  let name = "Pete";
+
+  return function() {
+    alert(name);
+  };
+}
+
+let name = "John";
+
+// create a function
+let work = makeWorker();
+
+// call it
+work(); // what will it show?
+Which value it will show? “Pete” or “John”?
+ */
+
+function makeWorker() {
+  let nama = 'Pete';
+
+  return function () {
+    console.log(nama); //Pete
+  };
+}
+
+let nama = 'John'; // variable nama ini baru akan diambil apabila di dalam function makeWorker() tidak ada variable nama
+
+// create a function
+let work = makeWorker();
+
+// call it
+work(); // what will it show?
