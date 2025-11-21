a6f9e6906f105ba3b6e180e1c15135ec587abb15
vetacode <vetacode@gmail.com>
2025-11-21 12:05:16 +0700

    adding real life case on new Function

--- newFunction.js
+++ newFunction.js
@@ -18,12 +18,38 @@ func(); //Joel
 // console.log(func());
 
 //function created using new Function, its [[Environment]] is set to reference not the current Lexical Environment, but the global one.
+//regular function:
 function getFunc() {
+  let value = 'test';
+
+  let func = function () {
+    console.log(value); //test
+  };
+
+  return func;
+}
+getFunc()(); // "test", from the Lexical Environment of getFunc
+
+//using new Function
+function getFunc2() {
   let value = 'test';
 
   let func = new Function('console.log(value)');
 
   return func;
 }
+// getFunc2()(); // error: value is not defined
 
-getFunc()(); // error: value is not defined
+//Contoh actual case: perbankan finance
+// data JSON dari server:
+/*
+{
+  "rule": "return user.age >= 18 && user.salary > 3000000;"
+}
+*/
+
+let ruleFunction = new Function('user', response.rule);
+
+let isValid = ruleFunction({ age: 20, salary: 5000000 });
+
+console.log(isValid);
