47d8e58ae172f06b3f4bdfc633db2655ffc585a3
vetacode <vetacode@gmail.com>
2025-11-21 13:05:16 +0700

    adding parameters to setTimeout

--- setTimeoutInterval.js
+++ setTimeoutInterval.js
@@ -3,5 +3,19 @@
 
 function hello() {
   console.log('Hai');
+  return 'Hai!!';
 }
-let setHello = setTimeout(hello, 3000);
+let setHello = setTimeout(hello, 2000);
+
+//Using parameters
+function sayHi(name, sureName, kabar) {
+  return `${hello()} ${name} ${sureName}, ${kabar}`; //kalo mau memasukkan function harus lgsg dijalankan () dlu
+}
+
+let sapa = setTimeout(sayHi, 2000, 'Justin', 'Bibir', 'Apo Kabare?');
+setTimeout(() => {
+  console.log(sayHi('Justin', 'Bibir', 'Apo Kabare?'));
+}, 2000);
+
+console.log(sayHi('Justin', 'Bibir', 'Apo Kabare?'));
+console.log(sapa);
