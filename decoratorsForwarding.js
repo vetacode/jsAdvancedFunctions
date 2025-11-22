5c115c4b1f384cdea8688d95b193a399cb8eba2a
vetacode <vetacode@gmail.com>
2025-11-22 10:53:47 +0700

    debugging cacheDecorator step by step

--- decoratorsForwarding.js
+++ decoratorsForwarding.js
@@ -19,9 +19,43 @@ function cachingDecorator(func) {
   };
 }
 
-slow = cachingDecorator(slow);
-console.log('data from cache:', slow(1));
-console.log('data from cache:', slow(2));
-console.log('data from cache:', slow(2));
-console.log('data from cache:', slow(2));
-console.log('data from cache:', slow(1));
+// slow = cachingDecorator(slow);
+// console.log('data from cache:', slow(1));
+// console.log('data from cache:', slow(2));
+// console.log('data from cache:', slow(2));
+// console.log('data from cache:', slow(2));
+// console.log('data from cache:', slow(1));
+
+function slow(x) {
+  //heavy CPU works function
+  console.log(
+    `this ${x} has been proceed from scratch, just 1 ${x} printed result below, afterwards the ${x} returned was from cache`
+  );
+  return x;
+}
+
+function decoratorCache(func) {
+  let cache = new Map();
+  // console.log(cache);
+  return function (x) {
+    // console.log(x);
+    if (cache.has(x)) {
+      return cache.get(x); //Jika fungsi if ini true, maka akan lgsg return (dan exit) argumen x yg udah disimpan di cache sebelumnya, sehingga fungsi else (fungsi setelah ini) tidak akan dijalankan
+    }
+
+    let result = func(x);
+    // console.log(result);
+    cache.set(x, result);
+    // console.log(x);
+    // console.log(result);
+    return result;
+  };
+}
+
+slow = decoratorCache(slow);
+console.log(slow('SATU'));
+console.log(slow('SATU'));
+console.log(slow('SATU'));
+console.log(slow(2));
+console.log(slow(2));
+// console.log(slow(3));
