291983b256aad939cff773c0fa7445fad681820a
vetacode <vetacode@gmail.com>
2025-11-23 17:14:27 +0700

    adding Bind

--- functionBinding.js
+++ functionBinding.js
@@ -9,5 +9,21 @@ let user = {
 setTimeout(user.sayHi, 1000); //Hello undefined
 user.sayHi();
 
-//SOLUTION => using function wrapper
+//SOLUTION
+// 1. using function wrapper
 setTimeout(() => user.sayHi(), 1000);
+
+//2. BIND
+//it results special function-like  "exotic object"
+// Syntax: let boundFunc = func.bind(context);
+
+let cust = {
+  name: 'Gugi',
+};
+
+function sayHey(phrase) {
+  console.log(phrase + ', ' + this.name + '!!'); //Gugi
+}
+
+let boundFunc = sayHey.bind(cust);
+boundFunc('Hallo'); // Argument will go to the original function parameter sayHey()
