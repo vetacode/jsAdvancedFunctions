f503733f17b37a8ec82757b0469b509a20b79bd5
vetacode <vetacode@gmail.com>
2025-11-24 11:42:35 +0700

    arrow function advanced completed

--- arrowFunctionAdv.js
+++ arrowFunctionAdv.js
@@ -34,20 +34,37 @@ group2.showList();
 //Arrow functions doesn't create BINDing, coz doesn't have this.
 //Arrow functions also have NO ARGUMENTS variable. => ini berguna untuk decorator, ketika ingin forward call menggunakan current this & arguments
 
+//Example using regular function: has its own arguments, so this couldnt borrow arguments from lex env => undefined
 function defer(f, ms) {
+  return function () {
+    setTimeout(function () {
+      f.apply(this, arguments);
+    }, ms);
+  };
+}
+
+function sayHi(who) {
+  console.log('Hello, ' + who); //Hello, undefined
+}
+
+let sayHiDefferred = defer(sayHi, 2000);
+sayHiDefferred('Angel');
+
+//USING ARROW FUNCTION, it works borrowing arguments from lex env above
+function defer2(f, ms) {
   return function () {
     setTimeout(() => f.apply(this, arguments), ms);
   };
 }
 
-function sayHi(who1, who2) {
-  console.log('Hello, ' + who1 + ' ' + who2);
+function sayHi2(who1, who2) {
+  console.log('Hello, ' + who1 + ' ' + who2); //Hello Angel Elgo
 }
 
-let sayHiDefferred = defer(sayHi, 2000);
-sayHiDefferred('Angel', 'Elgo');
+let sayHiDefferred2 = defer2(sayHi2, 2000);
+sayHiDefferred2('Angel', 'Elgo');
 
-//Using spread ...args
+//Using spread ...args => more modern syntax coz it returns real array
 function delay(f, ms) {
   return function (...args) {
     setTimeout(() => f.call(this, ...args), ms);
@@ -60,3 +77,5 @@ function sayHello(name1, name2) {
 
 let sayHelloDeffered = delay(sayHello, 1000);
 sayHelloDeffered('Tuti', 'Dudi');
+
+//arrow functions are excellent for decorators: they naturally forward this and arguments.
